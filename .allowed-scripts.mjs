c47df70d45b256aeaf06cca23b4956441c89f9c3
Patrick O'Donnell <patrick.odonnell@justice.gov.uk>
2025-12-19 10:57:02 +0000

    Updated allowed-scripts for Cypress 15.8.1

--- .allowed-scripts.mjs
+++ .allowed-scripts.mjs
@@ -3,7 +3,7 @@ import { configureAllowedScripts } from '@ministryofjustice/hmpps-npm-script-all
 export default configureAllowedScripts({
    allowlist: {
    "node_modules/@parcel/watcher@2.5.1": "ALLOW",
-   "node_modules/cypress@15.4.0": "ALLOW",
+   "node_modules/cypress@15.8.1": "ALLOW",
    "node_modules/esbuild@0.25.6": "ALLOW",
    "node_modules/fsevents@2.3.3": "ALLOW",
    "node_modules/playwright/node_modules/fsevents@2.3.2": "ALLOW",
