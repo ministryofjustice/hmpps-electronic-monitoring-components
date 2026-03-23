6120f7a70cae284278fce3d14eacb222ccf8142f
PaddyJustice <PaddyJustice@users.noreply.github.com>
2026-03-23 16:45:47 +0000

    deploy: 8a7b8ec0221e4f70b885d46b7479122d0c919750

---
+++ marker-tests/assets/formatter-EIJCOSYU-BLTD-Pfw.js
@@ -0,0 +1,1 @@
+import{d as i,c as m}from"./blocks-BMc59X84.js";import{b as o}from"./iframe-8-YCzoqj.js";import"./preload-helper-PPVm8Dsz.js";var t=o(m(),1),d=(0,t.default)(2)(async(e,r)=>e===!1?r:i(r));export{d as formatter};
