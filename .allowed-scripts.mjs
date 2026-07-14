a052b4d35fca49bcee4327a51c8614a986f48fa2
Patrick O'Donnell <patrick.odonnell@justice.gov.uk>
2026-07-14 17:24:10 +0100

    Updated allowed scripts versions

--- .allowed-scripts.mjs
+++ .allowed-scripts.mjs
@@ -3,10 +3,10 @@ import { configureAllowedScripts } from '@ministryofjustice/hmpps-npm-script-all
 export default configureAllowedScripts({
   allowlist: {
     "node_modules/@parcel/watcher@2.5.6": "ALLOW",
-    "node_modules/cypress@15.14.2": "ALLOW",
-    "node_modules/esbuild@0.27.7": "ALLOW",
+    "node_modules/cypress@15.18.1": "ALLOW",
+    "node_modules/esbuild@0.28.1": "ALLOW",
     "node_modules/fsevents@2.3.3": "ALLOW",
     "node_modules/playwright/node_modules/fsevents@2.3.2": "ALLOW",
-    "node_modules/unrs-resolver@1.11.1": "ALLOW"
+    "node_modules/unrs-resolver@1.12.2": "ALLOW"
   },
 })
